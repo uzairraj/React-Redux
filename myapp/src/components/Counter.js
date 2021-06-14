9cf137a858b8dcf5c63a6829593233b57ea27b84
uzair <uzairali896@gmail.com>
2021-06-14 15:30:13 +0500

    working with multiple states and properties

--- myapp/src/components/Counter.js
+++ myapp/src/components/Counter.js
@@ -4,6 +4,7 @@ import classes from './Counter.module.css';
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector(state=>state.counter);
+  const show = useSelector(state=>state.showCounter )
 
   const incrementHandler = () => {
 
@@ -25,7 +26,7 @@ const Counter = () => {
 
 
   const toggleCounterHandler = () => {
-  
+    dispatch({type:'toggle'});
 
 
   };
@@ -33,7 +34,7 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+     {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>increment</button>
         <button onClick={increaseHandler}>increaseby5</button>
--- myapp/src/store/index.js
+++ myapp/src/store/index.js
@@ -1,23 +1,36 @@
 
 import { createStore } from 'redux';
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState =  { counter: 0,showCounter:true};
+
+const counterReducer = (state =initialState, action) => {
   if (action.type === 'increment') {
     return {
       counter: state.counter + 1,
+      showCounter:state.showCounter
     };
   }
   //using payload actions
   if (action.type === 'incrementby5') {
     return {
       counter: state.counter + action.amount,
+      showCounter:state.showCounter
     };
   }
 
   if (action.type === 'decrement') {
     return {
       counter: state.counter - 1,
+      showCounter:state.showCounter
     };
+  }
+  if (action.type === 'toggle'){
+   return {
+    showCounter: false,
+    counter:state.counter
+
+   };
+
   }
 
   return state;
