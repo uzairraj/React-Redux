6b8656fb04c2285176096130dbe49f7cbe652682
uzair <uzairali896@gmail.com>
2021-06-14 18:16:34 +0500

    migrating to redux toolkit by export and import counterActions

--- myapp/src/components/Counter.js
+++ myapp/src/components/Counter.js
@@ -1,44 +1,38 @@
-import {useSelector,useDispatch} from 'react-redux';
+  
+import { useSelector, useDispatch } from 'react-redux';
+
+import { counterActions } from '../store/index';
 import classes from './Counter.module.css';
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector(state=>state.counter);
-  const show = useSelector(state=>state.showCounter )
+  const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
-
-    dispatch({type:'increment'});
-
+    dispatch(counterActions.increment());
   };
-  //using payload action
+
   const increaseHandler = () => {
-
-    dispatch({type:'incrementby5',amount:5});
-
+    dispatch(counterActions.increase(10)); // { type: SOME_UNIQUE_IDENTIFIER, payload: 10 }
   };
 
-  const decrementHandler = () =>{
-    
-    dispatch({type:'decrement'});
-
+  const decrementHandler = () => {
+    dispatch(counterActions.decrement());
   };
-
 
   const toggleCounterHandler = () => {
-    dispatch({type:'toggle'});
-
-
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-     {show && <div className={classes.value}>{counter}</div>}
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
-        <button onClick={incrementHandler}>increment</button>
-        <button onClick={increaseHandler}>increaseby5</button>
-        <button onClick={decrementHandler}>decrement</button>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 10</button>
+        <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
--- myapp/src/store/index.js
+++ myapp/src/store/index.js
@@ -1,36 +1,30 @@
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-import { createStore } from 'redux';
-import {createSlice,configureStore} from '@reduxjs/toolkit';
+const initialState = { counter: 0, showCounter: true };
 
-const initialState =  { counter: 0,showCounter:true};
- const counterSlice= createSlice({
- name:'counter',
- initialState,
- reducers:{
-   increment(state){
-     state.counter++;
-   },
-
-   decrement(state){
-     state.counter--;
-   },
-
-   increase(state,action){
-   state.counter= state.counter + action.amount;
-   },
-   toggleHandler(){
-   state.showCounter=!state.showCounter;
-
-   }
- }
-})
-;
-
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    }
+  }
+});
 
 const store = configureStore({
-
-  reducer:{counter:counterSlice.reducer}
-  
+  reducer: counterSlice.reducer
 });
+
+export const counterActions = counterSlice.actions;
 
 export default store;
