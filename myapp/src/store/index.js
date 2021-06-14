bb17c2d1f62a25853ba015d73b20db8dc518e448
uzair <uzairali896@gmail.com>
2021-06-14 20:30:48 +0500

    working with multipe slices reading ad dispatching

--- myapp/src/store/index.js
+++ myapp/src/store/index.js
@@ -1,11 +1,13 @@
 import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-const initialState = { counter: 0, showCounter: true };
+const initialCounterState = { counter: 0, showCounter: true, };
 
 const counterSlice = createSlice({
   name: 'counter',
-  initialState,
+  initialState:initialCounterState,
   reducers: {
+
+   
     increment(state) {
       state.counter++;
     },
@@ -20,11 +22,31 @@ const counterSlice = createSlice({
     }
   }
 });
+const initialAuthState = {
+  isAuthenticated:false
+}
+
+   const authSlice = createSlice({
+  name:'Auth',
+  initialState:initialAuthState,
+  reducers:{
+  login(state){
+    state.isAuthenticated = true;
+
+  },
+  logout(state){
+
+    state.isAuthenticated = false;
+  }
+
+  }
+
+});
 
 const store = configureStore({
-  reducer: counterSlice.reducer
+  reducer:{counter:counterSlice.reducer,auth:authSlice.reducer}
 });
 
 export const counterActions = counterSlice.actions;
-
+export const authActions = authSlice.actions;
 export default store;
